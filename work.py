c065c01cc49fa2a9ee6716a6725b829bea956a41
Eesuola Opeyemi Peter <86619956+eesuola@users.noreply.github.com>
2026-03-24 19:12:34 +0100

    Second Assignment solution

---
+++ work.py
@@ -0,0 +1,51 @@
+#Data Types Assignment
+name = "Ada"
+age = 12
+height = 12.4
+is_student = False
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#String concatenation Assignment
+first_name ="John"
+last_name="Doe"
+
+print(first_name + ' ' +last_name)
+
+#Mathematical Operations Assignment
+a =10
+b =3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#Floating point limitation Assignment
+from decimal import Decimal
+a = 0.1
+b =0.2
+
+#Floating point limitation Assignment
+a= Decimal ("0.1")
+b= Decimal ("0.2")
+
+print(a+b)
+#Truthy and Falsy assignment
+bool(0) truthy
+bool(1) truthy
+bool("") truthy
+bool("python") falsy
+bool(0.1) falsy
+bool(3.14) falsy
+bool(False) truthy
+bool(True) truthy
+
+##
